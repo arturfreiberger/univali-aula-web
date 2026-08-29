4ac1109953ffc31d334171bc15a48a333a9f11a8
arturfreiberger <arturnfreiberger89@gmail.com>
2026-08-28 21:09:49 -0300

    aula 4 - exercicios

--- aula 4/assets/scripts/resumo.js
+++ aula 4/assets/scripts/resumo.js
@@ -8,4 +8,16 @@ const carregarEndereco = function () {
     document.getElementById('cep').innerText = endereco.cep;
 }
 
+const carregarProdutos = function () {
+    let produtos = JSON.parse(
+        sessionStorage.getItem('produtos')
+    );
+    document.getElementById('produto1').innerText = produtos.havaianas;
+    document.getElementById('quantidade1').innerText = produtos.havaianas.quantidade;
+    document.getElementById('preco1').innerText = produtos.havaianas.preco;
+    document.getElementById('produto2').innerText = produtos.alpargatas;
+    document.getElementById('quantidade2').innerText = produtos.alpargatas.quantidade;
+    document.getElementById('preco2').innerText = produtos.alpargatas.preco;
+}
+
 carregarEndereco();
